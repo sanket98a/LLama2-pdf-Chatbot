5a2c644ec28dcfd675b58cf26d5a05fd852b1c22
sanket98a <bodake871@gmail.com>
2023-08-31 18:46:12 +0530

    files added

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def load_llm():
     model_id='TheBloke/Llama-2-7B-Chat-GGML'
     model_basename="llama-2-7b-chat.ggmlv3.q4_0.bin"
     max_ctx_size = 2048
-    n_gpu_layers = 90
-    n_batch = 1000
+    n_gpu_layers = 70
+    n_batch = 800
     model_path = hf_hub_download(repo_id=model_id, filename=model_basename)
 
     llm = LlamaCpp(
